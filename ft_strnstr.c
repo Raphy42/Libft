84d935b3771c7c7c2a54ff5b66a9c8c85e33fea1
Raphael DANTZER <rdantzer@e1r4p15.42.fr>
2014-11-24 22:19:06 +0100

    plop

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: rdantzer <rdantzer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/11 13:15:41 by rdantzer          #+#    #+#             */
-/*   Updated: 2014/11/19 19:10:45 by rdantzer         ###   ########.fr       */
+/*   Updated: 2014/11/24 22:16:30 by rdantzer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
 	char	*str;
 	size_t	len;
-	int		i;
+	size_t	i;
 
 	i = 0;
 	str = (char *)s1;
 	len = ft_strlen(s2);
 	if (n < len)
 		return (NULL);
-	while (i < (int)n)
+	while (i < n)
 	{
 		if (strncmp(str, s2, len) == 0)
 			return (str);
--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: rdantzer <rdantzer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/10 11:42:44 by rdantzer          #+#    #+#             */
-/*   Updated: 2014/11/12 16:09:02 by rdantzer         ###   ########.fr       */
+/*   Updated: 2014/11/24 22:18:13 by rdantzer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char		*ft_strsub(char const *s, unsigned int start, size_t len)
 	char	*p;
 
 	p = ft_strnew(len + 1);
-	if (s && len)
+	if (s && (int)len > 0)
 	{
 		ft_strncpy(p, s + start, len);
 		return (p);
